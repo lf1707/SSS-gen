0a4e1afee575f4958779cf931ab13e76c013c1a3
franks lum <lf1707@hotmail.com>
2018-10-10 15:56:45 +0800

    Update sss-gen.sh

--- bin/sss-gen.sh
+++ bin/sss-gen.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+# Auto generate the sss server docker, with random protocol, random port, random password, kcptun on/off, fast_open on/off, 
+# end of life period $date_eol from input $2;
+# usage:
+#   ./sss-gen.sh {$user_id} {$date_eol}
+#
